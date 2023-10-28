259b471950d399c33f3a6cf64cbc87345627802b
Romeo John Perez <rumyoonomicon@gmail.com>
2023-10-28 22:31:22 +0800

    Update CharacterMovement_Dodge Dash.cs
    - Implemented a Cooldown for the TP and the Dashing (Goodjob Romeo)

--- Assets/Scripts/CharacterMovement_Dodge Dash.cs
+++ Assets/Scripts/CharacterMovement_Dodge Dash.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-
-    // State Machine
-    private enum State {
+    private enum State
+    {
         Normal,
         Rolling
     }
@@ -17,98 +16,126 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] private Vector3 moveDirection;
     [SerializeField] private float moveSpeed = 2f;
 
-    // i hate math physics
     [Header("Player Blink")]
     [SerializeField] private float blinkAmount = 3f;
     private bool isDashButtonDown;
+    private float blinkCooldown = 2f; // Cooldown for Blink
+    private float blinkTimer;
 
     [Header("Player Dodge Roll")]
-    
     [SerializeField] private Vector3 rollDirection;
     [SerializeField] private float rollSpeed = 7f;
     private State state;
-
-    
+    private float rollCooldown = 1f; // Cooldown for Dodge Roll
+    private float rollTimer;
 
     void Start()
     {
-        
+        blinkTimer = 3f; // Initialize the Blink cooldown timer
+        rollTimer = 1f; // Initialize the Dodge Roll cooldown timer
     }
 
-    void Awake() {
+    void Awake()
+    {
         rb = GetComponent<Rigidbody2D>();
         state = State.Normal;
     }
 
-    // Update is called once per frame
     void Update()
     {
         GetInput();
         Blink();
     }
 
-    void FixedUpdate() {
-        switch (state) {
-        case State.Normal:
-            rb.velocity = moveDirection * moveSpeed;
+    void FixedUpdate()
+    {
+        switch (state)
+        {
+            case State.Normal:
+                rb.velocity = moveDirection * moveSpeed;
 
-            if (isDashButtonDown) {
-                rb.MovePosition(transform.position + moveDirection * blinkAmount);
-                isDashButtonDown = false;
-            }
-                
-            break;
-        case State.Rolling:
-            rb.velocity = rollDirection * rollSpeed;
-            break;
+                if (isDashButtonDown && blinkTimer <= 0)
+                {
+                    rb.MovePosition(transform.position + moveDirection * blinkAmount);
+                    isDashButtonDown = false;
+                    blinkTimer = blinkCooldown; // Set the Blink cooldown timer
+                }
+
+                break;
+            case State.Rolling:
+                rb.velocity = rollDirection * rollSpeed;
+                break;
+        }
+
+        // Update the cooldown timers
+        if (blinkTimer > 0)
+        {
+            blinkTimer -= Time.deltaTime;
+        }
+
+        if (rollTimer > 0)
+        {
+            rollTimer -= Time.deltaTime;
         }
     }
 
-    void GetInput() {
-        switch (state) {
-        case State.Normal:
-            float moveX = 0f;
-            float moveY = 0f;
+    void GetInput()
+    {
+        switch (state)
+        {
+            case State.Normal:
+                float moveX = 0f;
+                float moveY = 0f;
 
-            if (Input.GetKey(KeyCode.W)) {
-                moveY = 1f;
-            }
-            if (Input.GetKey(KeyCode.S)) {
-                moveY = -1f;
-            }
-            if (Input.GetKey(KeyCode.A)) {
-                moveX = -1f;
-            }
-            if (Input.GetKey(KeyCode.D)) {
-                moveX = 1f;
-            }
+                if (Input.GetKey(KeyCode.W))
+                {
+                    moveY = 1f;
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    moveY = -1f;
+                }
+                if (Input.GetKey(KeyCode.A))
+                {
+                    moveX = -1f;
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    moveX = 1f;
+                }
 
-            moveDirection = new Vector3(moveX, moveY).normalized;
-            DodgeRoll();
-            break;
-        case State.Rolling:
-            float rollSpeedDropMultiplier = 5f;
-            rollSpeed -= rollSpeed * rollSpeedDropMultiplier * Time.deltaTime;
+                moveDirection = new Vector3(moveX, moveY).normalized;
+                DodgeRoll();
+                break;
+            case State.Rolling:
+                float rollSpeedDropMultiplier = 5f;
+                rollSpeed -= rollSpeed * rollSpeedDropMultiplier * Time.deltaTime;
 
-            float rollSpeedMinimum = 1f;
-            if (rollSpeed < rollSpeedMinimum) {
-                state = State.Normal;
-            }
-            break;
+                float rollSpeedMinimum = 1f;
+                if (rollSpeed < rollSpeedMinimum)
+                {
+                    state = State.Normal;
+                }
+                break;
         }
     }
 
-    void Blink() {
-        if (Input.GetKey(KeyCode.F)) {
+    void Blink()
+    {
+        if (Input.GetKey(KeyCode.F) && blinkTimer <= 0)
+        {
             isDashButtonDown = true;
         }
     }
 
-    void DodgeRoll() {
-        if (Input.GetKey(KeyCode.Space)) {
+    void DodgeRoll()
+    {
+        if (Input.GetKey(KeyCode.Space) && rollTimer <= 0)
+        {
             rollDirection = moveDirection;
             rollSpeed = 5;
             state = State.Rolling;
+            rollTimer = rollCooldown; // Set the Dodge Roll cooldown timer
         }
     }
 }
