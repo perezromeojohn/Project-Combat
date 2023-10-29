a5fe2d5e871df7664c8479e628b866098eb59de0
Romeo John Perez <rumyoonomicon@gmail.com>
2023-10-29 15:35:01 +0800

    0.5 - Attack System
    - Made the sword look at the cursor
    - Added hitbox
    - Fixed Z indexing behind player

--- Assets/Scripts/SwordFollowsCursor.cs
+++ Assets/Scripts/SwordFollowsCursor.cs
@@ -10,6 +10,7 @@ public class SwordFollowsCursor : MonoBehaviour
     [SerializeField] private GameObject cursorGraph;
     [SerializeField] private SpriteRenderer playerSprite;
     [SerializeField] private SpriteRenderer hairSprite;
+    [SerializeField] private SpriteRenderer handSprite;
     void Start()
     {
         Cursor.visible = false;
@@ -25,19 +26,26 @@ public class SwordFollowsCursor : MonoBehaviour
         // make this object rotate towards the cursor's position, we are working with 2d
         // so we only need to worry about the z axis
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
         Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         cursorGraph.transform.position = cursorPos;
 
-        // flip the player sprite based on the cursor position
         if (cursorPos.x < transform.position.x) {
             playerSprite.flipX = true;
             hairSprite.flipX = true;
+            handSprite.transform.localScale = new Vector3(1f, -1f, 1f);
         } else {
             playerSprite.flipX = false;
             hairSprite.flipX = false;
+            handSprite.transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        if (transform.eulerAngles.z > 0 && transform.eulerAngles.z < 180) {
+            handSprite.sortingOrder = 9;
+        } else {
+            handSprite.sortingOrder = 13;
         }
     }
 }
