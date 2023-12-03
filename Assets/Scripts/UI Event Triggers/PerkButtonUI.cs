57b4d2538c4d6cbdf68037db96dbdd80194ced2b
Romeo John Perez <rumyoonomicon@gmail.com>
2023-12-03 17:52:02 +0800

    Updated GUI
    - Learned about TriggerEvents
    - Applied Hover and Exit events to the Perks Select Buttons

---
+++ Assets/Scripts/UI Event Triggers/PerkButtonUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerkButtonUI : MonoBehaviour
+{
+    private Vector3 scale;
+    void Start() {
+        // get gameObject vector3 scale
+        scale = gameObject.transform.localScale;
+    }
+    public void hoverPerkButton()
+    {
+        // Debug.Log("Hovering over perk button");
+        // get this gameobject's rectransform scale x and y, and then leantween increment tween it with + .1
+        LeanTween.scale(gameObject, new Vector3(scale.x + .1f, scale.y + .1f, scale.z), .1f).setEaseOutQuad();
+    }
+
+    public void exitsPerkButton()
+    {
+        // Debug.Log("Exiting perk button");
+        // revert the scale back to normal
+        LeanTween.scale(gameObject, scale, .1f).setEaseOutQuad();
+    }
+}
