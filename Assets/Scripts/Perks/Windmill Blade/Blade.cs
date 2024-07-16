2e43114bbf9d594d7d738a589f9511f1902faca6
Romeo John Perez <rumyoonomicon@gmail.com>
2024-07-16 23:21:40 +0800

    Windmill Skill

---
+++ Assets/Scripts/Perks/Windmill Blade/Blade.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    [SerializeField] private GameObject blade;
+    private Transform player;
+    private float maxSpeedMultiplier = 3f;
+    public float stayDuration = 1f;
+    public float travelDuration = 1f;
+    public float range = 3f;
+    public float speedIncreaseRate = 0.4f;
+    public float damage = 0f;
+    public float bladeSize = 1f;
+    private Vector3 targetPosition;
+
+    private List<Collider2D> hitEnemies = new List<Collider2D>();
+
+    void Start()
+    {
+        // find player
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        transform.localScale = Vector3.zero;
+        var newBladeSize = new Vector3(bladeSize, bladeSize, bladeSize); // idk why I did this
+        LeanTween.scale(gameObject, newBladeSize, 0.4f).setEase(LeanTweenType.easeOutBack);
+
+        Vector2 randomDirection = Random.insideUnitCircle.normalized;
+        targetPosition = player.position + new Vector3(randomDirection.x, randomDirection.y, 0) * range;
+
+        StartCoroutine(ThrowBoomerang());
+        StartCoroutine(ResetHitEnemiesList());
+    }
+
+    void Update()
+    {
+        blade.transform.Rotate(0, 0, -760 * Time.deltaTime);
+    }
+
+    IEnumerator ThrowBoomerang()
+    {
+        Vector3 startPosition = transform.position;
+        yield return MoveToPosition(startPosition, targetPosition, travelDuration);
+        yield return new WaitForSeconds(stayDuration);
+        yield return ReturnToPlayer(targetPosition);
+        Destroy(gameObject);
+    }
+
+    IEnumerator MoveToPosition(Vector3 from, Vector3 to, float duration)
+    {
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / duration);
+            t = EaseInOutQuad(t);
+            transform.position = Vector3.Lerp(from, to, t);
+            yield return null;
+        }
+        transform.position = to;
+    }
+
+    IEnumerator ReturnToPlayer(Vector3 from)
+    {
+        float initialSpeed = Vector3.Distance(from, player.position) / travelDuration;
+        float currentSpeed = initialSpeed;
+        float elapsedTime = 0f;
+        float maxSpeed = initialSpeed * maxSpeedMultiplier;
+
+        while (Vector3.Distance(transform.position, player.position) > 0.1f)
+        {
+            elapsedTime += Time.deltaTime;
+            
+            if (elapsedTime >= 1f)
+            {
+                if (currentSpeed < maxSpeed)
+                {
+                    currentSpeed += speedIncreaseRate;
+                }
+                elapsedTime = 0f;
+            }
+
+            float step = currentSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+            yield return null;
+        }
+        transform.position = player.position;
+    }
+
+    float EaseInOutQuad(float t)
+    {
+        return t < 0.5f ? 2f * t * t : 1f - Mathf.Pow(-2f * t + 2f, 2f) / 2f;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!hitEnemies.Contains(other))
+        {
+            if (other.gameObject.CompareTag("Enemy"))
+            {
+                hitEnemies.Add(other);
+                other.GetComponent<Behavior>().isAttacked = true;
+                other.GetComponent<Behavior>().damageTaken = damage;
+            }
+        }
+    }
+
+    IEnumerator ResetHitEnemiesList()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(.3f);
+            hitEnemies.Clear();
+        }
+    }
+}
---
+++ Assets/Scripts/Perks/Windmill Blade/WindmillBlade.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindmillBlade : MonoBehaviour
+{
+    private PerkManager perkManager;
+    public Perks perk;
+    private SkillCooldown cooldownScript;
+
+    [Header("Windmill Blade Components")]
+    private int projectileCount = 1;
+    private float skillDamage = 10;
+    private float cooldown = 0;
+    private float calculatedDamage;
+    public float damageMultiplier = 1.2f;
+    public GameObject projectilePrefab;
+    public float stayduration = 0.5f;
+    public float range = 1f;
+
+    void Start()
+    {
+        perkManager = GameObject.Find("PerksManager").GetComponent<PerkManager>();
+        cooldown = perk.perkCooldown;
+        cooldownScript = GetComponent<SkillCooldown>();
+    }
+
+    void Update()
+    {
+        if (cooldownScript != null && cooldownScript.cooldownTimer <= 0 && cooldownScript.isOnCooldown == false)
+        {
+            if (perkManager.playerPerks.ContainsKey(perk.perkName))
+            {
+                int perkLevel = perkManager.playerPerks[perk.perkName];
+                ActivateSkill(perkLevel);
+                cooldownScript.StartCooldown(cooldown); // Start the cooldown after activating the skill
+            }
+            else
+            {
+                Debug.Log("Eggbomb not found");
+            }
+        }
+    }
+
+    void ActivateSkill(int skillLevel)
+    {
+        // Calculate the damage multiplier based on the skill level
+        float damageMultiplier = 1f + (0.2f * (skillLevel - 1));
+
+        // Calculate the damage using the base damage and the calculated damage multiplier
+        calculatedDamage = skillDamage * damageMultiplier;
+
+        // Additional logic for other properties based on the skill level
+        switch(skillLevel)
+        {
+            case 2:
+                projectileCount = 1;
+                break;
+            case 3:
+                projectileCount = 2;
+                break;
+            case 4:
+                projectileCount = 3;
+                range = 1f;
+                break;
+            case 5:
+                projectileCount = 4;
+                break;
+        }
+        CastSkill(Mathf.Floor(calculatedDamage), projectileCount, range);
+        calculatedDamage = 0;
+    }
+
+    void CastSkill(float damage, int projectileCount, float projRange)
+    {
+        // // instantiate the projectile prefab
+        // GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        // // get the blade component from the projectile
+        // Blade blade = projectile.GetComponent<Blade>();
+        // // set the projectile count and range
+        // blade.projectileCount = projectileCount;
+        // blade.range = projRange;
+
+        for (int i = 0; i < projectileCount; i++)
+        {
+            GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            Blade blade = projectile.GetComponent<Blade>();
+            blade.range = projRange;
+            blade.stayDuration = stayduration;
+            blade.damage = damage;
+        }
+    }
+}
