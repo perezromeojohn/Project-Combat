168ade2996b256cc1f9981f9f84e9ae94fd81b0e
Romeo John Perez <rumyoonomicon@gmail.com>
2024-07-23 17:00:32 +0800

    Chest Stats

--- Assets/Scripts/LootChest/Chest.cs
+++ Assets/Scripts/LootChest/Chest.cs
@@ -19,19 +19,52 @@ public class Chest : MonoBehaviour
         chosenStat = stats[Random.Range(0, stats.Length)];
     }
 
-    void Update()
+    void IncreaseStats()
     {
-
-    }
-
-    void OpenChest()
-    {
-
-    }
-
-    void CheckIfPerkIsMaxed()
-    {
-
+        // get chosen stat Stat Name and look for it in the player stats
+        // if found, increase by 10%
+        for (int i = 0; i < stats.Length; i++)
+        {
+            if (chosenStat.statName == stats[i].statName)
+            {
+                if (chosenStat.statName == "movementSpeed")
+                {
+                    playerStats.movementSpeed += playerStats.movementSpeed * chosenStat.baseIncreaseAmount;
+                }
+                else if (chosenStat.statName == "magnetRange")
+                {
+                    playerStats.magnetRange += playerStats.magnetRange * chosenStat.baseIncreaseAmount;
+                }
+                else if (chosenStat.statName == "damage")
+                {
+                    playerStats.damage += playerStats.damage * chosenStat.baseIncreaseAmount;
+                }
+                else if (chosenStat.statName == "critChance")
+                {
+                    playerStats.critChance += playerStats.critChance * chosenStat.baseIncreaseAmount;
+                }
+                else if (chosenStat.statName == "critDamage")
+                {
+                    playerStats.critDamage += playerStats.critDamage * chosenStat.baseIncreaseAmount;
+                }
+                else if (chosenStat.statName == "attackSpeed")
+                {
+                    playerStats.attackSpeed += playerStats.attackSpeed * chosenStat.baseIncreaseAmount;
+                }
+                else if (chosenStat.statName == "knockbackStrength")
+                {
+                    playerStats.knockbackStrength += playerStats.knockbackStrength * chosenStat.baseIncreaseAmount;
+                }
+                else if (chosenStat.statName == "rollSpeed")
+                {
+                    playerStats.rollSpeed += playerStats.rollSpeed * chosenStat.baseIncreaseAmount;
+                }
+                else if (chosenStat.statName == "rollCooldown")
+                {
+                    playerStats.rollCooldown += playerStats.rollCooldown * chosenStat.baseIncreaseAmount;
+                }
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -40,6 +73,7 @@ public class Chest : MonoBehaviour
         {
             isCollected = true;
             feedbacks.PlayFeedbacks();
+            IncreaseStats();
         }
     }
 }
